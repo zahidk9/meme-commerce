c4c12c72f764bc5b09f274f1a61881f0755a9f5f
Callen <calljad@gmail.com>
2021-04-01 00:49:57 +0100

    Added urls, still to add urls for viewmeme/memeid and viewmeme/buymeme/memeid account/edit and account/memes are the only urls so far where the name isn't exactly the same as the path

--- memecommerce/urls.py
+++ memecommerce/urls.py
@@ -4,5 +4,12 @@ from memecommerce import views
 app_name = 'memecommerce'
 
 urlpatterns = [
-	path('', views.cow, name='cow')
+	path('', views.cow, name='cow'),
+	path('about/', views.about, name='about'),
+	path('sellmeme', views.sellmeme, name='sellmeme'),
+	path('account', views.account, name='account'),
+	path('account/edit', views.editAccount, name='editAccount'),
+	path('account/memes', views.myMemes, name='myMemes'),
+	path('login', views.login, name='login'),
+	path('register', views.register, name='register')
 ]
