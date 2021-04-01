e9b7d1c52923fbfc96e5ed96fb2f93898fc40bfd
Callen <calljad@gmail.com>
2021-04-02 00:09:40 +0100

    re-committed login view & imports

--- memecommerce/views.py
+++ memecommerce/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
 from memecommerce.models import Meme
 from django.http import HttpResponse
 from memecommerce.forms import UserForm, UserProfileForm
+from django.urls import reverse
 
 # Create your views here.
 
@@ -70,9 +72,41 @@ def myMemes(request):
 
 # authentication-related views
 def login(request):
-    context_dict = {}
-    response = render(request, 'memecommerce/login.html', context=context_dict)
-    return response
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+        # request.POST.get('<variable>') returns None if the
+        # value does not exist, while request.POST['<variable>']
+        # will raise a KeyError exception.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return redirect(reverse('memecommerce:home'))
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your Meme-Commerce account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'memecommerce/login.html')
 
 def register(request):
     registered = False
