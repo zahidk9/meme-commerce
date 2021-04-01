c9fcc8faa3dd38bda99c82b6b207080205dced65
Callen <calljad@gmail.com>
2021-04-01 18:49:28 +0100

    added blank pages for urls and simple view functions for each, still to add urls which use unique meme id

--- memecommerce/views.py
+++ memecommerce/views.py
@@ -3,6 +3,42 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-# Just a temporary view function for the homepage
 def home(request):
-    return render(request, 'memecommerce/home.html')
+    context_dict = {}
+    response = render(request, 'memecommerce/home.html', context=context_dict)
+    return response
+
+def about(request):
+    context_dict = {}
+    response = render(request, 'memecommerce/about.html', context=context_dict)
+    return response
+
+def sellMeme(request):
+    context_dict = {}
+    response = render(request, 'memecommerce/sellMeme.html', context=context_dict)
+    return response
+
+def account(request):
+    context_dict = {}
+    response = render(request, 'memecommerce/account.html', context=context_dict)
+    return response
+
+def editAccount(request):
+    context_dict = {}
+    response = render(request, 'memecommerce/editAccount.html', context=context_dict)
+    return response
+
+def myMemes(request):
+    context_dict = {}
+    response = render(request, 'memecommerce/myMemes.html', context=context_dict)
+    return response
+
+def login(request):
+    context_dict = {}
+    response = render(request, 'memecommerce/login.html', context=context_dict)
+    return response
+
+def register(request):
+    context_dict = {}
+    response = render(request, 'memecommerce/register.html', context=context_dict)
+    return response
