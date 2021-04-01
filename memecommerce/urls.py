9a2bc5300df892b72b11914277860c2ed598d6b3
Zahid Khan <2404574k@student.gla.ac.uk>
2021-04-01 17:39:51 +0100

    small change in urls.py

--- memecommerce/urls.py
+++ memecommerce/urls.py
@@ -5,11 +5,11 @@ app_name = 'memecommerce'
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	path('about/', views.about, name='about'),
-	path('sellmeme', views.sellmeme, name='sellmeme'),
-	path('account', views.account, name='account'),
-	path('account/edit', views.editAccount, name='editAccount'),
-	path('account/memes', views.myMemes, name='myMemes'),
-	path('login', views.login, name='login'),
-	path('register', views.register, name='register')
+	#path('about/', views.about, name='about'),
+	#path('sellmeme', views.sellmeme, name='sellmeme'),
+	#path('account', views.account, name='account'),
+	#path('account/edit', views.editAccount, name='editAccount'),
+	#path('account/memes', views.myMemes, name='myMemes'),
+	#path('login', views.login, name='login'),
+	#path('register', views.register, name='register')
 ]
