9b77f5d21528643d1a2f7c474cd8b73700a2ba62
Zahid Khan <2404574k@student.gla.ac.uk>
2021-04-04 03:31:17 +0100

    home page displays some pages

--- memecommerce_project/urls.py
+++ memecommerce_project/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from memecommerce import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('memecommerce/', include('memecommerce.urls')),
     path('admin/', admin.site.urls)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #from memecommerce.views import MainView, PostJsonListView
 	#path('', MainView.as_view(), name='main-view'),
     #path('posts-json/<int:num_posts>/', PostJsonListView.as_view(), name='posts-json-view'),
